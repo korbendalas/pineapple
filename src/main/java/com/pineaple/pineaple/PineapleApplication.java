63195c79299e805de0732a38a2e097daf7df0c82
dalibor <daliborpetric99@gmail.com>
2026-04-13 19:38:40 +0200

    Add whole files

---
+++ src/main/java/com/pineaple/pineaple/PineapleApplication.java
@@ -0,0 +1,13 @@
+package com.pineaple.pineaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PineapleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PineapleApplication.class, args);
+	}
+
+}
